574a18aa38009a9311a3068c1ad328272d80b1d8
RokkyStudio <rokkystudio@gmail.com>
2025-07-11 21:12:11 +0300

    Добавьте файлы проекта.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace GoProRename
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.ButtonFolder = new System.Windows.Forms.Button();
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.TextBoxFolder = new System.Windows.Forms.TextBox();
+            this.ButtonRename = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonFolder
+            // 
+            this.ButtonFolder.BackColor = System.Drawing.Color.White;
+            this.ButtonFolder.Image = ((System.Drawing.Image)(resources.GetObject("ButtonFolder.Image")));
+            this.ButtonFolder.Location = new System.Drawing.Point(369, 12);
+            this.ButtonFolder.Name = "ButtonFolder";
+            this.ButtonFolder.Size = new System.Drawing.Size(37, 30);
+            this.ButtonFolder.TabIndex = 0;
+            this.ButtonFolder.UseVisualStyleBackColor = false;
+            this.ButtonFolder.Click += new System.EventHandler(this.ButtonFolder_Click);
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "OpenFileDialog";
+            this.OpenFileDialog.RestoreDirectory = true;
+            // 
+            // TextBoxFolder
+            // 
+            this.TextBoxFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextBoxFolder.Location = new System.Drawing.Point(12, 12);
+            this.TextBoxFolder.MinimumSize = new System.Drawing.Size(200, 30);
+            this.TextBoxFolder.Name = "TextBoxFolder";
+            this.TextBoxFolder.Size = new System.Drawing.Size(351, 30);
+            this.TextBoxFolder.TabIndex = 1;
+            this.TextBoxFolder.TextChanged += new System.EventHandler(this.TextBoxFolder_TextChanged);
+            // 
+            // ButtonRename
+            // 
+            this.ButtonRename.BackColor = System.Drawing.Color.White;
+            this.ButtonRename.Enabled = false;
+            this.ButtonRename.Image = global::GoProRename.Properties.Resources._2;
+            this.ButtonRename.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ButtonRename.Location = new System.Drawing.Point(12, 55);
+            this.ButtonRename.Margin = new System.Windows.Forms.Padding(0);
+            this.ButtonRename.Name = "ButtonRename";
+            this.ButtonRename.Padding = new System.Windows.Forms.Padding(130, 0, 130, 0);
+            this.ButtonRename.Size = new System.Drawing.Size(394, 30);
+            this.ButtonRename.TabIndex = 2;
+            this.ButtonRename.Text = "Process Folder";
+            this.ButtonRename.UseVisualStyleBackColor = false;
+            this.ButtonRename.Click += new System.EventHandler(this.ButtonRename_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 97);
+            this.Controls.Add(this.ButtonRename);
+            this.Controls.Add(this.TextBoxFolder);
+            this.Controls.Add(this.ButtonFolder);
+            this.Name = "FormMain";
+            this.Text = "RenameGoPro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonFolder;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.TextBox TextBoxFolder;
+        private System.Windows.Forms.Button ButtonRename;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GoProRename
+{
+    public partial class FormMain : Form
+    {
+        public FormMain(string[] args) {
+            InitializeComponent();
+            TextBoxFolder.AutoSize = false;
+            ProcessCmdArgs(args);
+        }
+
+        private void ButtonFolder_Click(object sender, EventArgs e) {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath)) {
+                TextBoxFolder.Text = dialog.SelectedPath;
+                ButtonRename.Enabled = true;
+            }
+        }
+
+        private void ButtonRename_Click(object sender, EventArgs e) {
+            ProcessFolder(TextBoxFolder.Text);
+            ButtonRename.Enabled = false;
+        }
+
+        private void ProcessCmdArgs(string[] args) {
+            if (args != null && args.Length > 0) {
+                string path = args[0];
+                if (Directory.Exists(path)) {
+                    ProcessFolder(path);
+                }
+
+                Close();
+            }
+        }
+
+        private void ProcessFolder(string path) {
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                if (!IsGoProFile(file)) continue;
+                FileInfo info = new FileInfo(file);
+                info.MoveTo(GetRenamedFile(file));
+            }
+        }
+
+        private bool IsGoProFile(string file) {
+            string ext = Path.GetExtension(file).ToUpper();
+            if (ext != ".MP4" && ext != ".LRV" && ext != ".THM") return false;
+            if (Path.GetFileName(file).Length != 12) return false;
+            return true;
+        }
+
+        private string GetRenamedFile(string file) {
+            string filename = Path.GetFileName(file);
+            string chapter = filename.Substring(2, 2);
+            string number = filename.Substring(4, 4);
+            string path = Path.GetDirectoryName(file);
+            return Path.Combine(path, number + chapter + "_" + filename);
+        }
+
+        private void TextBoxFolder_TextChanged(object sender, EventArgs e) {
+            ButtonRename.Enabled = Directory.Exists(TextBoxFolder.Text);
+        }
+    }
+}
